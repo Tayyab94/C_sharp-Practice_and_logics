efc25d5c790443dd7f757b8ca045a383b1257986
tayyab94 <tayyab.bhatti30@gmail.com>
2020-01-06 02:46:54 -0800

    Convert 1-D array into 2-D Array in very simple way

---
+++ c_Sharp/Convert_1D_into_2D.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace c_Sharp
+{
+   public class Convert_1D_into_2D
+    {
+
+        public int [] array{ get; set; }
+
+        public Convert_1D_into_2D()
+        {
+            array = new int[] { 1, 2, 4, 5 };
+        }
+
+
+        public void Conversion(int r1, int c1)
+        {
+            int i = 0;
+            int[,] two = new int[r1, c1];
+
+            for (int x = 0; x < r1; x++)
+            {
+                for (int y = 0; y < c1; y++)
+                {
+                    two[x, y] = array[i];
+                    i++;
+
+                    Console.WriteLine(two[x, y]);
+                }
+            }
+        }
+    }
+}
--- c_Sharp/Program.cs
+++ c_Sharp/Program.cs
@@ -9,15 +9,11 @@ namespace c_Sharp
 
         static unsafe void Main(string[] args)
         {
-            int a = 10;int b = 20;
-            int* x = &a;  //Store the Address of A into X-Pointer 
-            int* y = &b;   // Store the Address of B into y-Pointer
-            Console.WriteLine("Before Swapping {0} - {1}", a, b);
-            SwapNo no = new SwapNo();
-            no.swap(x, y); // Swapping the Twno No.
-            Console.WriteLine("after Swapping {0} - {1}", a, b);
-            Console.ReadKey();
+            Convert_1D_into_2D array = new Convert_1D_into_2D();
 
+            array.Conversion(2, 2);
+
+            Console.ReadKey();
         }
     }
 }

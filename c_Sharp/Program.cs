09c1cbacfe4eb0d3a35e40d47b9149bf6b5feef5
tayyab94 <tayyab.bhatti30@gmail.com>
2020-01-06 02:31:21 -0800

    Swap Two inters with the help of Ponter

---
+++ c_Sharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace c_Sharp
+{
+    class Program
+    {
+       
+
+
+        static unsafe void Main(string[] args)
+        {
+            int a = 10;int b = 20;
+            int* x = &a;  //Store the Address of A into X-Pointer 
+            int* y = &b;   // Store the Address of B into y-Pointer
+            Console.WriteLine("Before Swapping {0} - {1}", a, b);
+            SwapNo no = new SwapNo();
+            no.swap(x, y); // Swapping the Twno No.
+            Console.WriteLine("after Swapping {0} - {1}", a, b);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ c_Sharp/SwapNo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace c_Sharp
+{
+   public class SwapNo
+    {
+        public unsafe void swap(int* p, int* q)
+        {
+            int temp = *p;
+            *p = *q;
+            *q = temp;
+        }
+
+       
+    }
+}

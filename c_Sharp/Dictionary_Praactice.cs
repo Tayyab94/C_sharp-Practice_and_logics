f30a657f074698db026906413d4da7f6118f3d37
tayyab94 <tayyab.bhatti30@gmail.com>
2020-01-24 11:06:23 -0800

    Implement the generic List

--- c_Sharp/Dictionary_Praactice.cs
+++ c_Sharp/Dictionary_Praactice.cs
@@ -4,6 +4,25 @@ using System.Text;
 
 namespace c_Sharp
 {
+
+    public class ClassicCar
+    {
+        public string m_Make;
+        public string m_Model;
+
+        public int m_year;
+        public int m_value;
+
+        public ClassicCar(string make,string model, int year, int value)
+        {
+            this.m_Make = make;
+            this.m_Model = model;
+            this.m_year = year; this.m_value = value;
+        }
+        
+    }
+
+   
     class Dictionary_Praactice
     {
         static void Main(string[] args)
@@ -32,6 +51,41 @@ namespace c_Sharp
                 Console.WriteLine($"Key => {k.Key}, value =>{k.Value}");
             }
             Console.ReadKey();
+
+
+
+            List<ClassicCar> carList = new List<ClassicCar>();
+
+            populateData(carList);
+
+            Console.WriteLine($"Total Element => {carList.Count}");
+
+            //List<ClassicCar> alfa = carList.FindAll(s => s.m_Make.Contains("Alfa"));
+            //Console.WriteLine($"No of alfa {alfa.Count}");
+            List<ClassicCar> alfa = carList.FindAll(findAlfa);
+            Console.WriteLine($"No of alfa {alfa.Count}");
+
+        }
+
+        static bool findAlfa(ClassicCar classicCar)
+        {
+            if(classicCar.m_Make.Contains("Alfa"))
+            {
+                return true;
+            }
+            return false;
+        }
+        static void populateData(List<ClassicCar> theList)
+        {
+            theList.Add(new ClassicCar("Alfa Romeo", "Spider ovoid", 2001, 1500));
+            theList.Add(new ClassicCar("altf Romeo", "Spider ovoid", 2201, 2300));
+            theList.Add(new ClassicCar("Alfa Romeo", "Spider ovoid", 2401, 1500));
+            theList.Add(new ClassicCar("Alfa Romeo", "Spider Bhai", 2011, 152300));
+            theList.Add(new ClassicCar("Alfa", "Spider ovoid", 2101, 150120));
+            theList.Add(new ClassicCar("Akbar Romeo", "Spider ovoid", 2111, 150210));
+            theList.Add(new ClassicCar("Romeo", "Spider ovoid", 2001, 15050));
+            theList.Add(new ClassicCar("Alfa Romeo", "Spider ovoid", 2201, 15300));
+            theList.Add(new ClassicCar("Testing", "Spider testing void", 2401, 15200));
         }
     }
 }
